140ba0afc81c9b4666a2a1dccc25b2c12fbffd2a
Peter Hoch <peter.hoch@users.noreply.github.com>
2016-05-14 07:52:42 +0200

    refactoring

---
+++ DecisionTableCreator/TestCases/TestCasesRootProperties.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecisionTableCreator.DynamicTable;
+
+namespace DecisionTableCreator.TestCases
+{
+    public partial class TestCasesRoot
+    {
+        private DataTableView _conditionTable;
+
+        public DataTableView ConditionTable
+        {
+            get { return _conditionTable; }
+            set
+            {
+                _conditionTable = value;
+                OnPropertyChanged("ConditionTable");
+            }
+        }
+
+        private DataTableView _actionTable;
+
+        public DataTableView ActionTable
+        {
+            get { return _actionTable; }
+            set
+            {
+                _actionTable = value;
+                OnPropertyChanged("ActionTable");
+            }
+        }
+
+        private ObservableCollection<TestCase> _testCases;
+
+        public ObservableCollection<TestCase> TestCases
+        {
+            get { return _testCases; }
+            set
+            {
+                _testCases = value;
+                OnPropertyChanged("TestCases");
+            }
+        }
+
+        private ObservableCollection<ConditionObject> _conditions;
+
+        public ObservableCollection<ConditionObject> Conditions
+        {
+            get { return _conditions; }
+            set
+            {
+                _conditions = value;
+                OnPropertyChanged("Conditions");
+            }
+        }
+
+        private ObservableCollection<ActionObject> _actions;
+
+        public ObservableCollection<ActionObject> Actions
+        {
+            get { return _actions; }
+            set
+            {
+                _actions = value;
+                OnPropertyChanged("Actions");
+            }
+        }
+
+        public event ViewChangedDelegate ConditionsChanged;
+
+        public event ViewChangedDelegate ActionsChanged;
+
+        private void FireConditionsChanged()
+        {
+            ConditionsChanged?.Invoke();
+        }
+
+        private void FireActionsChanged()
+        {
+            ActionsChanged?.Invoke();
+        }
+
+
+    }
+}
---
+++ DecisionTableCreator/TestCases/TestCasesRootSampleTables.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTableCreator.TestCases
+{
+    public partial class TestCasesRoot
+    {
+        public static TestCasesRoot CreateSampleTable()
+        {
+            TestCasesRoot testCasesRoot = new TestCasesRoot();
+            testCasesRoot.CreateSampleTableInternal();
+
+            return testCasesRoot;
+        }
+
+        public static TestCasesRoot CreateSimpleTable()
+        {
+            TestCasesRoot testCasesRoot = new TestCasesRoot();
+            testCasesRoot.CreateSimpleTableInternal();
+
+            return testCasesRoot;
+        }
+
+        private void CreateSampleTableInternal()
+        {
+            int testCasesCount = 20;
+            int conditionCount = 5;
+            int actionCount = 5;
+
+            List<ObservableCollection<EnumValue>> lists = new List<ObservableCollection<EnumValue>>();
+            for (int list = 0; list < (conditionCount + actionCount); list++)
+            {
+                var subList = new ObservableCollection<EnumValue>();
+                lists.Add(subList);
+                bool isInvalid = true;
+                for (int idx = 1; idx < 10; idx++)
+                {
+                    subList.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", list, idx), isInvalid));
+                    isInvalid = false;
+                }
+            }
+
+            int listIndex = 0;
+            Conditions.Add(ConditionObject.Create(String.Format("Condition {0}", 0), ValueDataType.Bool));
+            for (int idx = 1; idx < conditionCount; idx++)
+            {
+                Conditions.Add(ConditionObject.Create(String.Format("Condition {0}", idx), lists[listIndex++]));
+            }
+
+            Actions.Add(ActionObject.Create(String.Format("Action1{0}", 0), ValueDataType.Bool));
+            for (int idx = 1; idx < actionCount; idx++)
+            {
+                Actions.Add(ActionObject.Create(String.Format("Action1{0}", idx), lists[listIndex++]));
+            }
+
+            CreateBasicColumnDescriptions();
+
+            for (int idx = 0; idx < testCasesCount; idx++)
+            {
+                AddTestCase();
+            }
+
+            PopulateRows(ConditionTable, Conditions, TestCases, TestCase.CollectionType.Conditions);
+            PopulateRows(ActionTable, Actions, TestCases, TestCase.CollectionType.Actions);
+        }
+
+        private void CreateSimpleTableInternal()
+        {
+            int testCasesCount = 10;
+            int conditionCount = 5;
+            int actionCount = 5;
+
+            List<ObservableCollection<EnumValue>> lists = new List<ObservableCollection<EnumValue>>();
+            int listNumber = 0;
+            int enumIdx = 0;
+            var subListSample = new ObservableCollection<EnumValue>();
+            lists.Add(subListSample);
+            subListSample.Add(new EnumValue(String.Format("{0}-Invalid-{1}", listNumber, enumIdx++), true, false, true));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-Don' t care-{1}", listNumber++, enumIdx++), false, true, false));
+            enumIdx = 0;
+            subListSample = new ObservableCollection<EnumValue>();
+            lists.Add(subListSample);
+            subListSample.Add(new EnumValue(String.Format("{0}-Invalid-{1}", listNumber, enumIdx++), true, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, true));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-Don' t care-{1}", listNumber++, enumIdx++), false, true, false));
+            enumIdx = 0;
+            subListSample = new ObservableCollection<EnumValue>();
+            lists.Add(subListSample);
+            subListSample.Add(new EnumValue(String.Format("{0}-Invalid-{1}", listNumber, enumIdx++), true, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", listNumber, enumIdx++), false, false, false));
+            subListSample.Add(new EnumValue(String.Format("{0}-Don' t care-{1}", listNumber++, enumIdx++), false, true, true));
+
+            for (int list = listNumber; list < 10; list++)
+            {
+                var subList = new ObservableCollection<EnumValue>();
+                lists.Add(subList);
+                bool isInvalid = true;
+                for (int idx = 1; idx < 10; idx++)
+                {
+                    subList.Add(new EnumValue(String.Format("{0}-EnumValue-{1}", list, idx), isInvalid));
+                    isInvalid = false;
+                }
+            }
+
+            int listIndex = 0;
+            //Conditions.Add(new ConditionObject(String.Format("Condition {0}", 0), ConditionActionBase.ConditionActionType.Bool));
+            for (int idx = 1; idx < conditionCount; idx++)
+            {
+                Conditions.Add(ConditionObject.Create(String.Format("Condition {0}", idx), lists[listIndex++]));
+            }
+
+            for (int idx = 1; idx < actionCount; idx++)
+            {
+                Actions.Add(ActionObject.Create(String.Format("Action1{0}", idx), lists[listIndex++]));
+            }
+
+            CreateBasicColumnDescriptions();
+
+            for (int idx = 0; idx < testCasesCount; idx++)
+            {
+                AddTestCase();
+            }
+
+            PopulateRows(ConditionTable, Conditions, TestCases, TestCase.CollectionType.Conditions);
+            PopulateRows(ActionTable, Actions, TestCases, TestCase.CollectionType.Actions);
+        }
+
+        public static TestCasesRoot CreatePrinterTrubbleshootingSample()
+        {
+            TestCasesRoot testCasesRoot = new TestCasesRoot();
+            testCasesRoot.CreatePrinterTrubbleshootingSampleInternal();
+            return testCasesRoot;
+        }
+        private void CreatePrinterTrubbleshootingSampleInternal()
+        {
+            var printerEnum = new ObservableCollection<EnumValue>() { new EnumValue("", true), new EnumValue("Yes"), new EnumValue("No"), };
+            Conditions.Add(ConditionObject.Create(String.Format("Printer does not print"), printerEnum));
+
+            var ledEnum = new ObservableCollection<EnumValue>() { new EnumValue("", true), new EnumValue("Yes"), new EnumValue("No"), };
+            Conditions.Add(ConditionObject.Create("An error led is flashing", ledEnum));
+
+            var unrecognizedEnum = new ObservableCollection<EnumValue>() { new EnumValue("", true), new EnumValue("Yes"), new EnumValue("No"), };
+            Conditions.Add(ConditionObject.Create("Printer is unrecognized", unrecognizedEnum));
+
+            var checkPowerEnum = new ObservableCollection<EnumValue>() { new EnumValue("", true), new EnumValue(""), new EnumValue("X") };
+            Actions.Add(ActionObject.Create(String.Format("Check the power cable"), checkPowerEnum));
+
+            var checkPinterCableEnum = new ObservableCollection<EnumValue>() { new EnumValue(""), new EnumValue("X") };
+            Actions.Add(ActionObject.Create(String.Format("Check the printer-computer cable"), checkPinterCableEnum));
+
+            var softwareInstalledEnum = new ObservableCollection<EnumValue>() { new EnumValue(""), new EnumValue("X") };
+            Actions.Add(ActionObject.Create(String.Format("Enshure printer software is installed"), softwareInstalledEnum));
+
+            var checkInkEnum = new ObservableCollection<EnumValue>() { new EnumValue(""), new EnumValue("X") };
+            Actions.Add(ActionObject.Create(String.Format("Check/replace ink"), checkInkEnum));
+
+            var checkPaperJamEnum = new ObservableCollection<EnumValue>() { new EnumValue(""), new EnumValue("X") };
+            Actions.Add(ActionObject.Create(String.Format("Check for paper jam"), checkPaperJamEnum));
+
+            CreateBasicColumnDescriptions();
+
+            PopulateRows(ConditionTable, Conditions, TestCases, TestCase.CollectionType.Conditions);
+            PopulateRows(ActionTable, Actions, TestCases, TestCase.CollectionType.Actions);
+        }
+
+    }
+}
